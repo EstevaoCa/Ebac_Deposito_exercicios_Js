5a9d8821c36ae4491b80c121758b86092523b46d
tevo.castro14@gmail.com <tevo.castro14@gmail.com>
2024-01-01 18:22:52 -0300

    Trocando Número da conta por saldo

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,8 +1,8 @@
-const form = document.getElementById('form-deposito');
-const saldoConta = document.getElementById('saldo-conta');
-const valorSaque = document.getElementById('valor-saque');
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
-let formEValido = false;
+const form = document.getElementById('form-saque');
+const saldoContaInput = document.getElementById('saldo-conta-input');
+const valorSaqueInput = document.getElementById('valor-saque-input');
+const nomeBeneficiarioInput = document.getElementById('nome-beneficiario-input');
+let formularioValido = false;
 
 function validarNome(nomeCompleto) {
   const nomeComoArray = nomeCompleto.split(' ');
@@ -16,51 +16,47 @@ function validarSaque(saldo, saque) {
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  const mensagemSucesso = `Saque de: <b>${valorSaque.value}</b> realizado com sucesso. Saldo atual: <b>${saldoConta.value - valorSaque.value}</b>;`;
+  const mensagemSucesso = `Saque de: <b>${valorSaqueInput.value}</b> realizado com sucesso. Saldo atual: <b>${saldoContaInput.value - valorSaqueInput.value}</b>;`;
 
-  formEValido = validarNome(nomeBeneficiario.value);
-  const saqueValido = validarSaque(saldoConta.value, valorSaque.value);
+  formularioValido = validarNome(nomeBeneficiarioInput.value);
+  const saqueValido = validarSaque(saldoContaInput.value, valorSaqueInput.value);
 
-  if (formEValido && saqueValido) {
-    const conteinerMensagemSucesso = document.querySelector('.success-message');
-    conteinerMensagemSucesso.innerHTML = mensagemSucesso;
-    conteinerMensagemSucesso.style.display = 'block';
+  if (formularioValido && saqueValido) {
+    const mensagemSucessoContainer = document.querySelector('.success-message');
+    mensagemSucessoContainer.innerHTML = mensagemSucesso;
+    mensagemSucessoContainer.style.display = 'block';
 
-    saldoConta.value = '';
-    valorSaque.value = '';
-    nomeBeneficiario.value = '';
+    saldoContaInput.value = '';
+    valorSaqueInput.value = '';
+    nomeBeneficiarioInput.value = '';
   } else {
-    if (!formEValido) {
-      nomeBeneficiario.classList.add('error');
+    if (!formularioValido) {
+      nomeBeneficiarioInput.classList.add('error');
     } else {
-      nomeBeneficiario.classList.remove('error');
+      nomeBeneficiarioInput.classList.remove('error');
     }
 
-    saldoConta.classList.add('error');
+    saldoContaInput.classList.add('error');
     document.querySelector('.error-message').style.display = 'block';
   }
 });
 
-nomeBeneficiario.addEventListener('keyup', function(e) {
-  formEValido = validarNome(nomeBeneficiario.value);
+nomeBeneficiarioInput.addEventListener('keyup', function(e) {
+  formularioValido = validarNome(nomeBeneficiarioInput.value);
 
-  if (!formEValido) {
-    nomeBeneficiario.classList.add('error');
+  if (!formularioValido) {
+    nomeBeneficiarioInput.classList.add('error');
     document.querySelector('.error-message').style.display = 'block';
   } else {
-    nomeBeneficiario.classList.remove('error');
+    nomeBeneficiarioInput.classList.remove('error');
     document.querySelector('.error-message').style.display = 'none';
   }
 });
 
-saldoConta.addEventListener('keyup', function(e) {
-  formEValido = validarNome(nomeBeneficiario.value);
+valorSaqueInput.addEventListener('input', function(e) {
+  valorSaqueInput.value = formatarNumero(valorSaqueInput.value);
+});
 
-  if (!formEValido) {
-    nomeBeneficiario.classList.add('error');
-  } else {
-    nomeBeneficiario.classList.remove('error');
-  }
-
-  document.querySelector('.error-message').style.display = 'none';
+saldoContaInput.addEventListener('input', function(e) {
+  saldoContaInput.value = formatarNumero(saldoContaInput.value);
 });
